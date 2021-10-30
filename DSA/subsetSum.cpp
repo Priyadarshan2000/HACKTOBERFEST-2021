020d68b67b1a866353bb59e507fdeea534cb3f7a
Amit Banerjee <65404108+amitbanerjee1999@users.noreply.github.com>
2021-10-30 21:42:38 +0530

    Update subsetSum.cpp

--- DSA/subsetSum.cpp
+++ DSA/subsetSum.cpp
@@ -1,62 +1,51 @@
-// 05 Partition Equal Subset Sum
+// 02 N Queen Problem
 
-/* Given an array arr[] of size N, check if it can be partitioned into
- two parts such that the sum of elements in both parts is the same.*/
- 
- 
+//You have to place N chess queens on an N×N chessboard so that no two queens attack each other
 
-#include <bits/stdc++.h>
+#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
+int x[10];
+int cnt=0;
+
+bool place(int k,int i){
+	for(int j=1;j<=k-1;j++){
+		if(x[j]==i || abs(k-j)==abs(i-x[j])) return false;
+	}
+		
+	return true;
+}
 
 
 
-int equalPartition(int n, int a[])
-    {
-
-        int sum = 0;
-    int i, j;
-
-    for (i = 0; i < n; i++)
-        sum += a[i];
- 
-    if (sum % 2 != 0)
-        return false;
- 
-    bool part[sum / 2 + 1];
-
-    for (i = 0; i <= sum / 2; i++) {
-        part[i] = 0;
-    }
-
- 
-    for (i = 0; i < n; i++) {
-        for (j = sum / 2; j >= a[i];
-             j--) { 
-            if (part[j - a[i]] == 1 || j == a[i])
-                part[j] = 1;
-        }
-    }
- 
-    return part[sum / 2];
-    }
-
-
-
+void NQueen(int n,int k){
+	for(int i=1;i<=n;i++){
+		if(place(k,i)){
+			x[k]=i;
+			if(k==n){
+				cnt++;
+				for(int j=1;j<=n;j++){
+					cout<<x[j]<<" ";
+				}
+				cout<<endl;
+			}
+			else
+				NQueen(n,k+1);
+		}
+	}
+	
+}
 
 int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int N;
-        cin>>N;
-        int arr[N];
-        for(int i = 0;i < N;i++)
-            cin>>arr[i];
-        
-        if(equalPartition(N, arr))
-            cout<<"YES\n";
-        else
-            cout<<"NO\n";
-    }
-    return 0;
+	int n;
+	cout<<"Enter queens number: ";
+	cin>>n;
+	
+	
+	cout<<"The queens position are: "<<endl<<endl;
+	NQueen(n,1);
+	cout<<"The counts are:"<<cnt<<endl;
+	return 0;
 }
+
+
